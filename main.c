08900ac54240213caef938650c1b682bbce8b6ce
Dee Dee Raye McCroskey <deedee.mccroskey@gmail.com>
2018-04-26 21:43:14 -0500

    Got rid of wrapper around insideCircle

--- main.c
+++ main.c
@@ -7,17 +7,21 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include "omp.h"
+#include <time.h>
+#include <stdlib.h>
+
 
 
 double estimatePi(int long numTosses);
+double insideCircle(long int numTosses);
 
 int main(int argc, const char * argv[]) {
-
+    
     long int estimation;
     printf("Enter your toss value:");
     scanf("%d", &estimation);
     
-    double pi_estimate = estimatePi(estimation);
+    double pi_estimate = insideCircle(estimation);
     
     printf("%f\n",pi_estimate);
     
@@ -26,29 +30,34 @@ int main(int argc, const char * argv[]) {
 }
 
 
-int insideCircle(long int block)
+double insideCircle(int long numTosses)
 {
     srand (time( NULL));
     
-    int number_in_circle = 0;
-    for(int i = 0; i < block; i++)
+    double number_in_circle = 0.0;
+    double x =0.0;
+    double y= 0.0;
+    double distance_squared = 0.0;
+    #pragma omp parallel for shared(numTosses) reduction (+:number_in_circle)
+    for(long int i = 0; i < numTosses; i++)
     {
-        double x =(double)rand()/RAND_MAX*2.0-1.0;
-        double y =(double)rand()/RAND_MAX*2.0-1.0;
+        x =(double)rand()/RAND_MAX*2.0-1.0;
+        y =(double)rand()/RAND_MAX*2.0-1.0;
         
-        double distance_squared = x*x + y*y;
+        distance_squared = x*x + y*y;
         if(distance_squared <=1)
             number_in_circle++;
     }
     
-    return number_in_circle;
+    return (double)(4.0* number_in_circle / (double)numTosses);
+   
 }
 
 
 double estimatePi(int long numTosses)
 {
     long int number_of_tosses =numTosses;
-         int blocks =8;
+    int blocks =8;
     long int block = number_of_tosses/blocks;
     
     int count =0;
